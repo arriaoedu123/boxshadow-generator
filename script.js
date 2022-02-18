cb873fc4b3de528455d75bbaf78f875da60cf78c
Aryon Rabello <73148019+arriaoedu123@users.noreply.github.com>
2022-02-18 18:17:07 -0300

    Update files

--- script.js
+++ script.js
@@ -5,17 +5,48 @@
 # Version: 1.0
 */
 
-const gradientBtn = document.querySelector(".gradient-button")
-const boxShadowBtn = document.querySelector(".box-shadow-button")
-const gradientGenerator = document.querySelector(".gradient-generator")
-const boxShadowGenerator = document.querySelector(".box-shadow-generator")
+const boxShadowPreview = document.querySelector('.box-shadow-preview')
+const radiusInput = document.querySelector('#radius')
+const horizontalInput = document.querySelector('#horizontal')
+const verticalInput = document.querySelector('#vertical')
+const spreadInput = document.querySelector('#spread')
+const blurInput = document.querySelector('#blur')
+const copyBtn = document.querySelector('.copy')
 
-gradientBtn.addEventListener("click", () => {
-	gradientGenerator.style.display = "block"
-	boxShadowGenerator.style.display = "none"
+const radiusCode = document.querySelector('.radius-code')
+const horizontalCode = document.querySelector('.horizontal-code')
+const verticalCode = document.querySelector('.vertical-code')
+const spreadCode = document.querySelector('.spread-code')
+const blurCode = document.querySelector('.blur-code')
+
+radiusInput.addEventListener('input', () => {
+	let radiusInputValue = radiusInput.value
+	boxShadowPreview.style.setProperty("--radius", `${radiusInputValue}px`)
+	radiusCode.innerHTML = `${radiusInputValue}px`
+
+	if (radiusInputValue == 130) { radiusCode.innerHTML = "50%" }
 })
 
-boxShadowBtn.addEventListener("click", () => {
-	gradientGenerator.style.display = "none"
-	boxShadowGenerator.style.display = "block"
+horizontalInput.addEventListener('input', () => {
+	let horizontalInputValue = horizontalInput.value
+	boxShadowPreview.style.setProperty("--horizontal", `${horizontalInputValue}px`)
+	horizontalCode.innerHTML = `${horizontalInputValue}px `
+})
+
+verticalInput.addEventListener('input', () => {
+	let verticalInputValue = verticalInput.value
+	boxShadowPreview.style.setProperty("--vertical", `${verticalInputValue}px`)
+	verticalCode.innerHTML = `${verticalInputValue}px `
+})
+
+spreadInput.addEventListener('input', () => {
+	let spreadInputValue = spreadInput.value
+	boxShadowPreview.style.setProperty("--spread", `${spreadInputValue}px`)
+	spreadCode.innerHTML = `${spreadInputValue}px `
+})
+
+blurInput.addEventListener('input', () => {
+	let blurInputValue = blurInput.value
+	boxShadowPreview.style.setProperty("--blur", `${blurInputValue}px`)
+	blurCode.innerHTML = `${blurInputValue}px `
 })
